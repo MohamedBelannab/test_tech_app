500567ce93da628d0a0acd6f13641d65fd9ccffa
Mohamed <MohamedBelannab@gmail.com>
2024-12-19 21:36:48 +0100

    config deployement

--- functions/api.mjs
+++ functions/api.mjs
@@ -2,19 +2,23 @@ import express from "express";
 import { readFile } from "fs/promises";
 import cors from "cors";
 import serverless from 'serverless-http';
+import { fileURLToPath } from 'url';
+import { dirname, join } from 'path';
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 // Create an Express app
 const app = express();
 
 // Enable CORS and JSON parsing
-app.use(cors("*"));
+app.use(cors());
 app.use(express.json());
 
 // Endpoint to get all products
 app.get("/.netlify/functions/api/products", async (req, res) => {
   try {
-    const data = await readFile("./database.json");
+    const data = await readFile(join(__dirname, "database.json"), "utf-8");
     res.json(JSON.parse(data));
   } catch (err) {
     console.error('Error reading products:', err);
@@ -27,7 +31,7 @@ app.get("/.netlify/functions/api/products/:id", async (req, res) => {
   const productId = parseInt(req.params.id, 10);
   
   try {
-    const data = await readFile("database.json");
+    const data = await readFile(join(__dirname, "database.json"), "utf-8");
     const products = JSON.parse(data);
     const product = products.find((p) => p.id === productId);
     
