54f49b508787683c5b5fae5357de40bf8af4d2b3
Mohamed <MohamedBelannab@gmail.com>
2024-12-19 21:52:16 +0100

    config deployement

--- functions/api.mjs
+++ functions/api.mjs
@@ -1,53 +1,46 @@
 import express from "express";
-import { readFile } from "fs/promises";
+import fs from "fs/promises";
 import cors from "cors";
-import serverless from 'serverless-http';
-import { fileURLToPath } from 'url';
-import { dirname, join } from 'path';
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+import serverless from 'serverless-http'; 
 
 // Create an Express app
 const app = express();
+const port = 5000; // local dev
 
-// Enable CORS and JSON parsing
+// Enable CORS
 app.use(cors("*"));
 app.use(express.json());
+
 
 // Endpoint to get all products
 app.get("/.netlify/functions/api/products", async (req, res) => {
   try {
-    const data = await readFile(join(__dirname, "database.json"), "utf-8");
-    res.json(JSON.parse(data));
+    // const data = await fs.readFile("database.json");
+    res.json(JSON.parse({"name" : "ok"}));
   } catch (err) {
-    console.error('Error reading products:', err);
-    return res.status(500).json({ message: "Error" });
-  }
-});
-
-// Endpoint to get a single product by ID
-app.get("/.netlify/functions/api/products/:id", async (req, res) => {
-  const productId = parseInt(req.params.id, 10);
-  
-  try {
-    const data = await readFile(join(__dirname, "database.json"), "utf-8");
-    const products = JSON.parse(data);
-    const product = products.find((p) => p.id === productId);
-    
-    if (!product) {
-      return res.status(404).json({ message: "Product not found" });
-    }
-    
-    res.json(product);
-  } catch (err) {
-    console.error('Error reading product:', err);
     return res.status(500).json({ message: "Error reading products file" });
   }
 });
 
-// Export the serverless handler
-export const handler = serverless(app);
-// app.listen(5000, () => {
-//   console.log(`Server is running on http://localhost:${5000}`);
+// Endpoint to get a single product by ID
+app.get("/api/products/:id", async (req, res) => {
+  const productId = parseInt(req.params.id, 10);
+  try {
+    const data = await fs.readFile(path.join(__dirname, "database.json"), "utf-8");
+    const products = JSON.parse(data);
+    const product = products.find((p) => p.id === productId);
+    if (!product) {
+      return res.status(404).json({ message: "Product not found" });
+    }
+    res.json(product);
+  } catch (err) {
+    return res.status(500).json({ message: "Error reading products file" });
+  }
+});
+
+// Start the server
+// app.listen(port, () => {
+//   console.log(`Server is running on http://localhost:${port}`);
 // });
+
+export const handler = serverless(app);
